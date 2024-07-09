1d718339599b1508639838411026bb81287b54f4
Ekaterina Akimenko <106630894+akimoshka@users.noreply.github.com>
2024-07-09 20:53:57 +0300

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
-
-let assetPrefix = "";
-let basePath = "";
-
-if (isGithubActions) {
-    const repo = process.env.My_portfolio ? process.env.My_portfolio.split('/')[1] : "";
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-const nextConfig = {
-  reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  trailingSlash: true,
-  images: {
-    unoptimized: true,
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
